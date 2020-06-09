22f8e85759f365068ac326c48208512560582dbe
estherjsuh <estherjsuh@gmail.com>
2020-06-09 12:50:58 -0700

    broke out homepage into smaller comonents; completed styling menuitem components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import MenuItem from './components/menu-item/menu-item.component';
 
 function App() {
   return (
