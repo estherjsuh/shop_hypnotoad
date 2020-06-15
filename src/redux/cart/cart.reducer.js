f33e8b81b48b9553bab01f51cb6552df7df99eb9
estherjsuh <estherjsuh@gmail.com>
2020-06-15 11:37:42 -0700

    updated cart utils to group items in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,21 +1,29 @@
 import CartActionTypes from './cart.types';
+import {addItemToCart} from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'TOGGLE_CART_HIDDEN':
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden //converts to opposite
-            }
+            };
+
+
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload) //cartItems get updated with existing items + new item (payload)
+        };
 
         default:
             return state;
     }
-
 };
 
 
