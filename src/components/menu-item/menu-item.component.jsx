e91bf324cd0074609f9fef8de691831ae1581d94
estherjsuh <estherjsuh@gmail.com>
2021-02-11 09:09:00 -0800

    Update homepage images

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -16,7 +16,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
 
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='sub-title'>SHOP NOW</span>
         
         </div>
     </div>
