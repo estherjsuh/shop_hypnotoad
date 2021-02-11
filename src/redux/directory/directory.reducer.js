7bb01bd6eb434f454a1e4760fc61fa2688b81ae5
estherjsuh <estherjsuh@gmail.com>
2021-02-11 09:44:52 -0800

    Add shop route, update homepage

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,33 +1,33 @@
 const INITIAL_STATE ={
     sections: [
         {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: 'bender',
+          imageUrl: 'https://i.ibb.co/nnxF6tL/bender.jpg',
           id: 1,
           linkUrl: 'shop/hats'
         },
         {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          title: 'fry',
+          imageUrl: 'https://i.ibb.co/56pm0JN/fry3.png',
           id: 2,
           linkUrl: 'shop/jackets'
         },
         {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          title: 'leila',
+          imageUrl: 'https://i.ibb.co/kmdj29w/leila.png',
           id: 3,
           linkUrl: 'shop/sneakers'
         },
         {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          title: 'zoidberg',
+          imageUrl: 'https://i.ibb.co/bsvjT9h/wallup-net.jpg',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
         },
         {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          title: 'planet express ship',
+          imageUrl: 'https://i.ibb.co/jW0PfPG/planetex.jpg',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens'
