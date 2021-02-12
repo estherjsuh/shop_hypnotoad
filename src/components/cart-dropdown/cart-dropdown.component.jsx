d9011327507c162d0bdbb67f96375e353f3dee50
estherjsuh <estherjsuh@gmail.com>
2021-02-12 09:40:26 -0800

    Enable outside click functionality to hide cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef,useEffect } from 'react';
 
 import {connect} from 'react-redux';
 import { createStructuredSelector} from 'reselect';
@@ -12,31 +12,47 @@ import CartItem from '../cart-item/cart-item.components';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartItems, history, dispatch}) => (
-    <div className='cart-dropdown'>
+import { useDispatch } from 'react-redux'
+
+
+
+function CartDropdown({cartItems, history,dispatch }) {
+    const dropDownRef = useRef(null)
+
+    useEffect(() => {
+        dropDownRef.current.focus()
+      }, [])
+    
+      return (
+        <div
+          className= 'cart-dropdown'
+          ref={dropDownRef}
+          tabIndex='0'
+          onBlur={() => {
+            dispatch(toggleCartHidden())
+          }}
+        >
         <div className='cart-items'>
-            {
-                cartItems.length ? (
-                    cartItems.map(cartItem => (
-                    <CartItem key={cartItem.id} item= {cartItem}/>
-            ))
-                ) : (
-                    <span className='empty-message'>Your cart is empty</span>
-                ) 
-            }
+        {
+            cartItems.length ? (
+                cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item= {cartItem}/>
+        ))
+            ) : (
+                <span className='empty-message'>Your cart is empty</span>
+            ) 
+        }
         </div>
-        <CustomButton onClick={() => {
+          <CustomButton onClick={() => {
             history.push('/checkout');
             dispatch(toggleCartHidden());
-        }}>Go to checkout</CustomButton>
-        
-
-    </div>
-)
+        }}>Go to checkout</CustomButton>        
+        </div>
+      )
+}
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
-
 });
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
